35c42070d3f93832baef6399d2e150185d17e329
Bruno de Moraes <61841052+theoriginalkaiju@users.noreply.github.com>
2021-05-15 22:15:29 -0300

    a

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { useEffect, useState } from 'react';
